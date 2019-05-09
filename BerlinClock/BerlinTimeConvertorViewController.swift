6eb72ab254447e701918341caacd03d6e2eb2ba4
2019_SOW_DEV_005 <2019SOWDEV005@gmail.com>
2019-05-09 19:34:47 +0200

    Renamed class viewController to BerlinTimeConvertorViewController

--- BerlinClock/BerlinTimeConvertorViewController.swift
+++ BerlinClock/BerlinTimeConvertorViewController.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-class ViewController: UIViewController {
+class BerlinTimeConvertorViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
