e5b1174d45522c7669499cdff8e8396c0e709495
2019_SOW_DEV_005 <2019SOWDEV005@gmail.com>
2019-05-09 21:05:18 +0200

    Moved berlinTimePresenter creation to setup method

--- BerlinClockTests/BerlinTimePresenter_Tests.swift
+++ BerlinClockTests/BerlinTimePresenter_Tests.swift
@@ -3,41 +3,37 @@ import XCTest
 
 class BerlinTimePresenter_Tests: XCTestCase {
     
+    private var berlinTimePresenter:BerlinTimePresenter!
+    
+    override func setUp() {
+        berlinTimePresenter = BerlinTimePresenter.init()
+    }
+    
     func test_ReturnTwentyFive_WhenComponentZeroReceived()  {
-        let berlinTimePresenter = BerlinTimePresenter.init()
-        
         let result = berlinTimePresenter.numberOfRowsInPickerView(0)
         
         XCTAssertEqual(25, result)
     }
     
     func test_ReturnOne_WhenComponentOneReceived()  {
-        let berlinTimePresenter = BerlinTimePresenter.init()
-        
         let result = berlinTimePresenter.numberOfRowsInPickerView(1)
         
         XCTAssertEqual(1, result)
     }
     
     func test_ReturnSixty_WhenComponentTwoReceived()  {
-        let berlinTimePresenter = BerlinTimePresenter.init()
-        
         let result = berlinTimePresenter.numberOfRowsInPickerView(2)
         
         XCTAssertEqual(60, result)
     }
     
     func test_ReturnOne_WhenComponentThreeReceived()  {
-        let berlinTimePresenter = BerlinTimePresenter.init()
-        
         let result = berlinTimePresenter.numberOfRowsInPickerView(3)
         
         XCTAssertEqual(1, result)
     }
     
     func test_ReturnSixty_WhenComponentFourReceived()  {
-        let berlinTimePresenter = BerlinTimePresenter.init()
-        
         let result = berlinTimePresenter.numberOfRowsInPickerView(4)
         
         XCTAssertEqual(60, result)
