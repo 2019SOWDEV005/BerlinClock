dffbed1a2cbb39187953ab39fb38e406b82649aa
2019_SOW_DEV_005 <2019SOWDEV005@gmail.com>
2019-05-09 19:05:22 +0200

    Implemented test test_BlinkSingleRedLightInFiveHoursRow_WhenFiveHoursReceived

--- BerlinClock/Model/BerlinTimeFiveHoursConverter.swift
+++ BerlinClock/Model/BerlinTimeFiveHoursConverter.swift
@@ -1,3 +1,16 @@
 class BerlinTimeFiveHoursConverter {
+    private let numberOfFiveHours:Int
+    private let maximumNumberOfLights = 4
     
+    init(_ digitalHours:DigitalHours) {
+        self.numberOfFiveHours = digitalHours.numberOFFiveHoursValue()
+    }
+    
+    func asBerlinTime() -> String {
+        var  berlinTime = ""
+        
+        berlinTime = berlinTime.padding(toLength: numberOfFiveHours, withPad: LightIndicator.asStringValue(.Red), startingAt: 0)
+        
+        return  berlinTime.padding(toLength: maximumNumberOfLights, withPad: LightIndicator.asStringValue(.Off), startingAt: 0)
+    }
 }
--- BerlinClockTests/BerlinTimeFiveHoursConverter_Tests.swift
+++ BerlinClockTests/BerlinTimeFiveHoursConverter_Tests.swift
@@ -2,4 +2,12 @@ import XCTest
 @testable import BerlinClock
 
 class BerlinTimeFiveHoursConverter_Tests: XCTestCase {
+    
+    func test_BlinkSingleRedLightInFiveHoursRow_WhenFiveHoursReceived() {
+        let berlinTimeFiveHoursConverter = BerlinTimeFiveHoursConverter.init(DigitalHours.init(5))
+        
+        let result = berlinTimeFiveHoursConverter.asBerlinTime()
+        
+        XCTAssertEqual("ROOO", result)
+    }
 }
