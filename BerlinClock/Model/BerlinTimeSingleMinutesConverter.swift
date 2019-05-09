5cfd80bb370c4c4aa37efdbbb63d9a7041c2440d
2019_SOW_DEV_005 <2019SOWDEV005@gmail.com>
2019-05-09 17:09:54 +0200

    Adopted LightIndicator Enum in BerlinTimeSingleMinutesConverter class

--- BerlinClock/Model/BerlinTimeSingleMinutesConverter.swift
+++ BerlinClock/Model/BerlinTimeSingleMinutesConverter.swift
@@ -2,9 +2,7 @@ class BerlinTimeSingleMinutesConverter {
     
     private let numberOfSingleMinutes:Int
     private let maximumNumberOfLights = 4
-    private let yellowLightRepresentation = "Y"
-    private let lightOffRepresentation = "O"
-    
+ 
     init(_ digitalMinutes:Int) {
         self.numberOfSingleMinutes = digitalMinutes % 5
     }
@@ -13,8 +11,8 @@ class BerlinTimeSingleMinutesConverter {
         
         var  singleMinutesRepresentation = ""
         
-        singleMinutesRepresentation = singleMinutesRepresentation.padding(toLength: numberOfSingleMinutes, withPad:yellowLightRepresentation, startingAt: 0)
+        singleMinutesRepresentation = singleMinutesRepresentation.padding(toLength: numberOfSingleMinutes, withPad:LightIndicator.asStringValue(.Yellow), startingAt: 0)
         
-        return  singleMinutesRepresentation.padding(toLength: maximumNumberOfLights, withPad: lightOffRepresentation, startingAt: 0)
+        return  singleMinutesRepresentation.padding(toLength: maximumNumberOfLights, withPad: LightIndicator.asStringValue(.Off), startingAt: 0)
     }
 }
