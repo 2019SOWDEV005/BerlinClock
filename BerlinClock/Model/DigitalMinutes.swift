1b54c39defa9e722113aa7ee63c777ad45615d59
2019_SOW_DEV_005 <2019SOWDEV005@gmail.com>
2019-05-09 17:29:00 +0200

    Moved calculate number of five minutes from the given minutes  logic to DigitalMinutes model

--- BerlinClock/Model/DigitalMinutes.swift
+++ BerlinClock/Model/DigitalMinutes.swift
@@ -1,11 +1,17 @@
 class DigitalMinutes {
     private let numberOfSingleMinutes:Int
-    
+    private let numberOfFiveMinutes:Int
+
     init(_ minutes:Int) {
         self.numberOfSingleMinutes = minutes % 5
+        self.numberOfFiveMinutes = minutes / 5
     }
     
     func numberOfSingleMinutesValue() -> Int {
         return numberOfSingleMinutes
+    }
+    
+    func numberOfFiveMinutesValue() -> Int {
+        return numberOfFiveMinutes
     }
 }
