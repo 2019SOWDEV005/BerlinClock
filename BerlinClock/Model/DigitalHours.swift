1fd00f9afcc131020daf149a00460b01a1c06fbb
2019_SOW_DEV_005 <2019SOWDEV005@gmail.com>
2019-05-09 18:21:01 +0200

    Implemented logic to calculate number of five hours from the given hours

--- BerlinClock/Model/DigitalHours.swift
+++ BerlinClock/Model/DigitalHours.swift
@@ -1,11 +1,17 @@
 class DigitalHours {
     private let numberOfSingleHours:Int
+    private let numberOfFiveHours:Int
  
     init(_ hours:Int) {
         self.numberOfSingleHours = hours % 5
+        self.numberOfFiveHours = hours / 5
      }
     
     func numberOfSingleHoursValue() -> Int {
         return numberOfSingleHours
+    }
+    
+    func numberOFFiveHoursValue() -> Int {
+        return numberOfFiveHours
     }
 }
