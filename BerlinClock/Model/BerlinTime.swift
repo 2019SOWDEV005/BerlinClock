000ed723a675eace33c4036cf627fe7063d9ba30
2019_SOW_DEV_005 <2019SOWDEV005@gmail.com>
2019-05-09 23:13:27 +0200

    BerlinTime model  object is with singleMinutes, Five Minutes, FiveHours, single Hours ,seconds option

--- BerlinClock/Model/BerlinTime.swift
+++ BerlinClock/Model/BerlinTime.swift
@@ -1,3 +1,19 @@
 class BerlinTime {
+    let secondsRowValue:String
+    let fiveHoursRowValue:String
+    let singleHoursRowValue:String
+    let fiveMinuteRowValue:String
+    let singleMinuteRowValue:String
     
+    init(secondsRowValue:String,
+         fiveHoursRowValue:String,
+         singleHoursRowValue:String,
+         fiveMinuteRowValue:String,
+         singleMinuteRowValue:String) {
+        self.secondsRowValue = secondsRowValue
+        self.fiveHoursRowValue = fiveHoursRowValue
+        self.singleHoursRowValue = singleHoursRowValue
+        self.fiveMinuteRowValue = fiveMinuteRowValue
+        self.singleMinuteRowValue = singleMinuteRowValue
+    }
 }
