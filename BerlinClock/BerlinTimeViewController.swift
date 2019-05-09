3aea4d11d0e28bc8566de1fcff40d46249f11093
2019_SOW_DEV_005 <2019SOWDEV005@gmail.com>
2019-05-09 20:17:54 +0200

    Implemented UIPickerViewDelegate methods

--- BerlinClock/BerlinTimeViewController.swift
+++ BerlinClock/BerlinTimeViewController.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 class BerlinTimeViewController: UIViewController {
-
+    
     @IBOutlet weak var txtDigitalTime: UITextField!
     
     override func viewDidLoad() {
@@ -9,10 +9,28 @@ class BerlinTimeViewController: UIViewController {
         createTimePicker()
     }
     
-   private func createTimePicker() {
+    private func createTimePicker() {
         let timePicker = UIPickerView()
-        timePicker.delegate = self as? UIPickerViewDelegate
+        timePicker.delegate = self
         txtDigitalTime.inputView = timePicker
-     }
+    }
+}
+
+extension BerlinTimeViewController:UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 0
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 0
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return nil
+    }
 }
 
