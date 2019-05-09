988cb852a35aba78c30ddb208d9c38461851ed74
2019_SOW_DEV_005 <2019SOWDEV005@gmail.com>
2019-05-09 17:20:06 +0200

    Moved numberOfSingleMinutes calculation from the given minutes to DigitalMinutes model class

--- BerlinClock/Model/DigitalMinutes.swift
+++ BerlinClock/Model/DigitalMinutes.swift
@@ -1,3 +1,11 @@
 class DigitalMinutes {
+    private let numberOfSingleMinutes:Int
     
+    init(_ minutes:Int) {
+        self.numberOfSingleMinutes = minutes % 5
+    }
+    
+    func numberOfSingleMinutesValue() -> Int {
+        return numberOfSingleMinutes
+    }
 }
