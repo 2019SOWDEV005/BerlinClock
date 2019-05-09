dc40a273b2ec700380fec204cb18d2f4445be7b8
2019_SOW_DEV_005 <2019SOWDEV005@gmail.com>
2019-05-10 01:31:00 +0200

    Tested scenario test_ShouldNotShowEnterValidInputError_WhenUserSelectsTheValueInPickerAndClickOnShowBerlinTimeButton

--- BerlinClockUITests/BerlinTimeViewController_UITests.swift
+++ BerlinClockUITests/BerlinTimeViewController_UITests.swift
@@ -15,4 +15,21 @@ class BerlinTimeViewController_UITests: XCTestCase {
         
         XCTAssertTrue(errorAlert.exists)
     }
+    
+    func test_ShouldNotShowEnterValidInputError_WhenUserSelectsTheValueInPickerAndClickOnShowBerlinTimeButton() {
+        
+        let app = XCUIApplication()
+        app.textFields["Select Digital Time"].tap()
+        
+        let pickerWheel = app.pickers.children(matching: .pickerWheel).matching(identifier: "00").element(boundBy: 0)
+        pickerWheel.tap()
+        pickerWheel.adjust(toPickerWheelValue: "18")
+
+        app.toolbars["Toolbar"].buttons["Done"].tap()
+        app.buttons["Show Berlin Time"].tap()
+        
+        let errorAlert = app.alerts["Error"]
+        XCTAssertFalse(errorAlert.exists)
+ 
+    }
 }
