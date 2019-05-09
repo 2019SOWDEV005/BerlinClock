bc3eeece66c0111f81777b084b36b4924886d873
2019_SOW_DEV_005 <2019SOWDEV005@gmail.com>
2019-05-09 16:32:31 +0200

    Implemented test scenario test_BlinkOneYellowLightInFiveMinutesRow_WhenFiveMinutesReceived

--- BerlinClock/Model/BerlinTimeFiveMinutesConverter.swift
+++ BerlinClock/Model/BerlinTimeFiveMinutesConverter.swift
@@ -1,3 +1,18 @@
 class BerlinTimeFiveMinutesConverter {
+    private let numberOfFiveMinutes: Int
+    private let maximumNumberOfLights = 11
+    private let yellowLightRepresentation = "Y"
+    private let lightOffRepresentation = "O"
     
+    init(_ digitalMinutes:Int) {
+        self.numberOfFiveMinutes = digitalMinutes / 5
+    }
+    
+    func asFiveMinutesRepresentation() -> String {
+        var fiveMinutesRepresentation = ""
+        
+        fiveMinutesRepresentation = fiveMinutesRepresentation.padding(toLength: numberOfFiveMinutes, withPad:yellowLightRepresentation, startingAt: 0)
+
+            return  fiveMinutesRepresentation.padding(toLength: maximumNumberOfLights, withPad: lightOffRepresentation, startingAt: 0)
+        }
 }
--- BerlinClockTests/BerlinTimeFiveMinutesConverter_Tests.swift
+++ BerlinClockTests/BerlinTimeFiveMinutesConverter_Tests.swift
@@ -3,4 +3,11 @@ import XCTest
 
 class BerlinTimeFiveMinutesConverter_Tests: XCTestCase {
     
+    func test_BlinkOneYellowLightInFiveMinutesRow_WhenFiveMinutesReceived() {
+        let berlinTimeFiveMinutesConverter = BerlinTimeFiveMinutesConverter.init(5)
+        
+        let result = berlinTimeFiveMinutesConverter.asFiveMinutesRepresentation()
+        
+        XCTAssertEqual("YOOOOOOOOOO", result)
+    }
 }
