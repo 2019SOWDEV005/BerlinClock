7003b8b97e1253acf733ab3420b0bde4dabc99c6
2019_SOW_DEV_005 <2019SOWDEV005@gmail.com>
2019-05-09 17:06:33 +0200

    Added method asStringValue to get the RawValue of LightIndicator Enum

--- BerlinClock/constants/BerlinClockConstants.swift
+++ BerlinClock/constants/BerlinClockConstants.swift
@@ -2,4 +2,8 @@ enum LightIndicator:String {
     case Yellow = "Y"
     case Red    = "R"
     case Off    = "O"
+    
+    static func asStringValue(_ lightIndicator:LightIndicator) -> String {
+        return lightIndicator.rawValue
+    }
 }
