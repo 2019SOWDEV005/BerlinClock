7df36ddc164d00102ff5578a0aa1aebc6da1d22e
2019_SOW_DEV_005 <2019SOWDEV005@gmail.com>
2019-05-09 22:20:47 +0200

    Created Hours and minutes and Seconds inside Digital Time class

--- BerlinClock/Model/DigitalTime.swift
+++ BerlinClock/Model/DigitalTime.swift
@@ -1,3 +1,23 @@
 class DigitalTime {
-    
+private let hours:DigitalHours
+private let minutes:DigitalMinutes
+private let seconds:Int
+
+init(hours:Int,minutes:Int,seconds:Int) {
+    self.hours = DigitalHours(hours)
+    self.minutes = DigitalMinutes(minutes)
+    self.seconds = seconds
+}
+
+func hoursValue() -> DigitalHours {
+    return hours
+}
+
+func minutesValue() -> DigitalMinutes {
+    return minutes
+}
+
+func secondsValue() -> Int {
+    return seconds
+}
 }
