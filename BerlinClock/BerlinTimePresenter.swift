10940d938f075de86a43a7c775fe2b8e21a4ef99
2019_SOW_DEV_005 <2019SOWDEV005@gmail.com>
2019-05-09 20:38:37 +0200

    Implemented test scenario test_ReturnTwentyFive_WhenComponentZeroReceived

--- BerlinClock/BerlinTimePresenter.swift
+++ BerlinClock/BerlinTimePresenter.swift
@@ -1,3 +1,9 @@
 class BerlinTimePresenter {
-    
+    func numberOfRowsInPickerView(_ component: Int) -> Int {
+        if component == 0 {
+            return 25
+        }
+        return 0
+    }
+
 }
--- BerlinClockTests/BerlinTimePresenter_Tests.swift
+++ BerlinClockTests/BerlinTimePresenter_Tests.swift
@@ -2,4 +2,12 @@ import XCTest
 @testable import BerlinClock
 
 class BerlinTimePresenter_Tests: XCTestCase {
+    
+    func test_ReturnTwentyFive_WhenComponentZeroReceived()  {
+        let berlinTimePresenter = BerlinTimePresenter.init()
+        
+        let result = berlinTimePresenter.numberOfRowsInPickerView(0)
+        
+        XCTAssertEqual(25, result)
+    }
 }
