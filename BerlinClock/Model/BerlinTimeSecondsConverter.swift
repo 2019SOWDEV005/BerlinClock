1123aaa1ab441687056c4a16d4c952800f217d53
2019_SOW_DEV_005 <2019SOWDEV005@gmail.com>
2019-05-10 00:13:08 +0200

    Implemented scenario test_BlinkYellowLightInSecondsRow_WhenEvenNumberOFSecondsReceived

--- BerlinClock/Model/BerlinTimeSecondsConverter.swift
+++ BerlinClock/Model/BerlinTimeSecondsConverter.swift
@@ -1,3 +1,19 @@
 class BerlinTimeSecondsConverter {
+    private let digitalSeconds:DigitalSeconds
     
+    init(_ digitalSeconds:DigitalSeconds) {
+        self.digitalSeconds = digitalSeconds
+    }
+    
+    func asBerlinTime() -> String {
+        
+        if isEvenSeconds(digitalSeconds) {
+            return LightIndicator.asStringValue(.Yellow)
+        }
+        return ""
+    }
+    
+    private func isEvenSeconds(_ seconds:DigitalSeconds) ->Bool {
+        return digitalSeconds.value() % 2 == 0
+    }
 }
--- BerlinClockTests/BerlinTimeSecondsConverter_Tests.swift
+++ BerlinClockTests/BerlinTimeSecondsConverter_Tests.swift
@@ -3,4 +3,12 @@ import XCTest
 
 class BerlinTimeSecondsConverter_Tests: XCTestCase {
     
+    func test_BlinkYellowLightInSecondsRow_WhenEvenNumberOFSecondsReceived() {
+        let berlinTimeSecondsConverter = BerlinTimeSecondsConverter.init(DigitalSeconds(10))
+        
+        let result = berlinTimeSecondsConverter.asBerlinTime()
+        
+        XCTAssertEqual(LightIndicator.asStringValue(.Yellow), result)
+    }
+    
 }
